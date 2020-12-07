5ecc0acac1bcf86328099296710741c2f27d9fec
dedoblege <dedoblege@gmail.com>
2020-12-07 13:30:02 +0100

    adding the navigation bar menu to the other pages

--- shared.js
+++ shared.js
@@ -13,18 +13,24 @@ for (var i = 0; i < selectPlanButton.length; i++) {
 }
 
 backdrop.addEventListener("click", function () {
-  mobileNav.style.display = "none"
+  mobileNav.classList.remove("open")
   closeModal()
 })
-
-modalNoButton.addEventListener("click", closeModal)
+if (modalNoButton) {
+  modalNoButton.addEventListener("click", closeModal)
+}
 
 toggleButton.addEventListener("click", function () {
-  mobileNav.style.display = "block"
-  backdrop.style.display = "block"
+  mobileNav.classList.add("open")
+  backdrop.classList.add("open")
 })
 
 function closeModal() {
-  modal.classList.remove("open")
-  backdrop.classList.remove("open")
+  if (modal) {
+    modal.classList.remove("open")
+  }
+
+  if (backdrop) {
+    backdrop.classList.remove("open")
+  }
 }
