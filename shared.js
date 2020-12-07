3832fa6aba65fac8bb02f7aee89523441e958b61
dedoblege <dedoblege@gmail.com>
2020-12-07 12:32:01 +0100

    adding styles with js (opening closing modal)

--- shared.js
+++ shared.js
@@ -1,1 +1,19 @@
-alert("This works!")
+var backdrop = document.querySelector(".backdrop")
+var modal = document.querySelector(".modal")
+var modalNoButton = document.querySelector(".modal__action--negative")
+var selectPlanButton = document.querySelectorAll(".plan button")
+
+for (var i = 0; i < selectPlanButton.length; i++) {
+  selectPlanButton[i].addEventListener("click", function () {
+    modal.style.display = "block"
+    backdrop.style.display = "block"
+  })
+}
+
+backdrop.addEventListener("click", closeModal)
+modalNoButton.addEventListener("click", closeModal)
+
+function closeModal() {
+  modal.style.display = "none"
+  backdrop.style.display = "none"
+}
