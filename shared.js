21e761e6f592f6ac271902b355cd9f63c989a54e
dedoblege <dedoblege@gmail.com>
2020-12-07 12:49:39 +0100

    opening and closing the navigation bar menu via js

--- shared.js
+++ shared.js
@@ -2,6 +2,8 @@ var backdrop = document.querySelector(".backdrop")
 var modal = document.querySelector(".modal")
 var modalNoButton = document.querySelector(".modal__action--negative")
 var selectPlanButton = document.querySelectorAll(".plan button")
+var toggleButton = document.querySelector(".toggle-button")
+var mobileNav = document.querySelector(".mobile-nav")
 
 for (var i = 0; i < selectPlanButton.length; i++) {
   selectPlanButton[i].addEventListener("click", function () {
@@ -10,8 +12,17 @@ for (var i = 0; i < selectPlanButton.length; i++) {
   })
 }
 
-backdrop.addEventListener("click", closeModal)
+backdrop.addEventListener("click", function () {
+  mobileNav.style.display = "none"
+  closeModal()
+})
+
 modalNoButton.addEventListener("click", closeModal)
+
+toggleButton.addEventListener("click", function () {
+  mobileNav.style.display = "block"
+  backdrop.style.display = "block"
+})
 
 function closeModal() {
   modal.style.display = "none"
